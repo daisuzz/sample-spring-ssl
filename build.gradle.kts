1c3d1a66cdfd6c3f62d18e0553477f6113f9d6bf
daisuzz <daisaku.rugby@gmail.com>
2023-08-05 11:50:02 +0900

    add dotenv

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("me.paulschwarz:spring-dotenv:4.0.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
